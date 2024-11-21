1221bc6a533f89f1b973eabe4df468554aebc9a2
Sava Popov <savawow1@abv.bg>
2024-11-21 15:28:20 +0200

    Fixed some bugs added guards and fixed home page

--- blog-posts/src/app/main/main.component.ts
+++ blog-posts/src/app/main/main.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { ThemesListComponent } from "../theme/themes-list/themes-list.component";
 import { PostsListComponent } from "../posts-list/posts-list.component";
+import { WelcomeMessageComponent } from "../shared/welcome-message/welcome-message.component";
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [ThemesListComponent, PostsListComponent],
+  imports: [ThemesListComponent, PostsListComponent, WelcomeMessageComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+  constructor(private userService: UserService) { }
 }
